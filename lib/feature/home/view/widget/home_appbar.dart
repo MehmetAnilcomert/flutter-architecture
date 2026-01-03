72c55f9a1b4e2bed467c93450777c851d3bb95bb
Mehmet Anıl <m.cmert1907@hotmail.com>
2026-01-03 23:00:19 +0300

    lint change

--- lib/feature/home/view/widget/home_appbar.dart
+++ lib/feature/home/view/widget/home_appbar.dart
@@ -1,7 +1,7 @@
 part of '../home_view.dart';
 
 class _HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
-  const _HomeAppbar({super.key});
+  const _HomeAppbar();
 
   @override
   Widget build(BuildContext context) {
