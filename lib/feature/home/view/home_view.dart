afca386db6d09405358ba30b710292b7924b2fa8
Mehmet Anıl <m.cmert1907@hotmail.com>
2026-01-04 17:53:49 +0300

    ProductLocalization class added to manage EasyLocalization package and decrease coupling.
    Locales enum specifies and restricts supported language`s locales

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -1,3 +1,7 @@
+import 'package:architecture_template/product/init/language/locale_keys.g.dart';
+import 'package:architecture_template/product/init/product_localization.dart';
+import 'package:architecture_template/product/utility/constants/enums/locales.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 part 'widget/home_appbar.dart';
@@ -14,7 +18,23 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const _HomeAppbar(),
-      body: const Center(child: Text('Welcome to the Home View!')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(LocaleKeys.app_title).tr(),
+            ElevatedButton(
+              onPressed: () {
+                ProductLocalization.updateLang(
+                  context: context,
+                  locale: Locales.tr.locale,
+                );
+              },
+              child: const Text('Change Language'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/product/init/language/locale_keys.g.dart
@@ -0,0 +1,18 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: constant_identifier_names
+
+abstract class  LocaleKeys {
+  static const app_title = 'app_title';
+  static const home_hello_world = 'home.hello_world';
+  static const home_welcome_message = 'home.welcome_message';
+  static const home = 'home';
+  static const general_ok = 'general.ok';
+  static const general_cancel = 'general.cancel';
+  static const general_yes = 'general.yes';
+  static const general_no = 'general.no';
+  static const general_save = 'general.save';
+  static const general_delete = 'general.delete';
+  static const general = 'general';
+
+}
